0db1ac3debacb2c0b4c5f38c77945213208d5c73
Szymon <szymon.wojcikowski@prokonto.pl>
2018-12-07 22:24:07 +0100

    checking if the tasks do not exceed the maximum working time & draggable feature

---
+++ js/app3.js
@@ -0,0 +1,292 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("taskInput");
+    const priorityInput = document.getElementById("priorityInput");
+    const daySelect = document.getElementById("daySelect");
+    const timeInput = document.getElementById("timeInput");
+    const taskListContainer = document.getElementsByClassName("day");
+    const addBtn = document.getElementById("addTaskButton");
+    const removeFinishedTasksBtn = document.getElementById("removeFinishedTasksButton");
+    const taskToDoCounter = document.getElementById("counter");
+
+    let taskList = [];
+    let idCounter = 0;
+
+    let dragging;
+    let draggingId;
+
+    //---settings
+    const activityHours = 16;
+    const sectionWeekHeight = 80;
+
+    //---konstruktor zadania
+    const Task = function (task, priority, id, day, time) {
+        this.priority = priority;
+        this.task = task;
+        this.id = id;
+        //-------
+        this.day = day;
+        this.time = time;
+    };
+    //---tablica zadan posortowana po kluczu priorytetu
+    const taskListSorted = function (taskList) {
+        let sortedTaskTab = taskList.slice().sort((a, b) => b.priority - a.priority);
+        //powyżej użycie .slice() aby stworzyć kopię tablicy, inaczej sortowało również tablicę źródłową
+        return sortedTaskTab;
+    };
+    //---czyszczenie listy danego dnia
+    const clearList = function (dayNr) {
+        //console.log(dayNr, typeof dayNr);
+        if (Array.isArray(dayNr)) {
+            for (let i = 0; i < dayNr.length; i++) {
+                //console.log("petla nr" + i, "argument dayNr " + dayNr, "długość arg " + dayNr.length, "HTML danego taska " + taskListContainer[i]);
+                for (let k = 0; k < taskListContainer.length; k++) {
+                    if (taskListContainer[k].dataset.day == dayNr[i]) {
+                        taskListContainer[k].innerHTML = "";
+                        //console.log("w warunku czyszczącym");
+                    }
+                }
+
+            }
+        }
+        else {
+            //console.log("var");
+            taskListContainer[dayNr].innerHTML = "";
+        }
+    };
+    //---wypelnianie zadaniami danej listy
+    const printList = function (dayNr) {
+        for (let i = 0; i < taskListSorted(taskList).length; i++) {
+            let nextTask = taskListSorted(taskList)[i].task;
+            let nextTaskId = taskListSorted(taskList)[i].id;
+            let nextTaskHeight = sectionWeekHeight/activityHours * taskListSorted(taskList)[i].time;
+            console.warn("nextTaskLength ",nextTaskHeight);
+            //------------------------------
+            let taskToAdd = document.createElement("li");
+            taskToAdd.draggable = true;
+            taskToAdd.classList.add("task");
+            taskToAdd.dataset.id = nextTaskId;
+            taskToAdd.style.height = nextTaskHeight + "vh";
+            taskToAdd.innerHTML += `<h1>${nextTask}</h1><button class="btn delete"><i class="fas fa-times"></i></button><button class="btn selected"><i class="fas fa-check"></i></button>`;
+            if (taskListSorted(taskList)[i].day === dayNr) {
+                taskListContainer[dayNr].appendChild(taskToAdd);
+                //odpalenie obslugi zdarzen dla wydrukowanych guzikow
+                buttonsInAddedTask(dayNr);
+                //console.log("print this task: " + taskList[i]);
+            } else {
+                for (let j = 0; j < dayNr.length; j++) {
+                    if (taskListSorted(taskList)[i].day === dayNr[j]) {
+                        taskListContainer[dayNr[j]].appendChild(taskToAdd);
+                        //odpalenie obslugi zdarzen dla wydrukowanych guzikow
+                        buttonsInAddedTask(dayNr[j]);
+                        //console.log("print this task: " + taskList[i]);
+                    }
+                }
+            }
+            dragEventsOfTask();
+        }
+    };
+    //---odswiezanie listy zadan z danego dnia
+    const taskListRefresh = function (dayNr) {
+        clearList(dayNr);
+        printList(dayNr);
+    };
+
+    //---odswiezanie licznika zadan
+    const TaskCounterRefresh = function () {
+        taskToDoCounter.innerText = taskList.length;
+    };
+    //---czyszczenie inputow
+    const inputsRefresh = function () {
+        taskInput.value = "";
+        priorityInput.value = "";
+        timeInput.value = "";
+    };
+    //---przyciski w zadaniach
+    const buttonsInAddedTask = function (dayNr) {
+        const getButtons = taskListContainer[dayNr].querySelectorAll("li>button"); //dayNr undefined
+        //---zdarzenia przyciskow
+        for (let i = 0; i < getButtons.length; i++) {
+            getButtons[i].addEventListener("click", function () {
+                //---klikniecie przycisku usuwania
+                let taskToDelete = [];
+                taskToDelete.push(parseInt(this.parentElement.dataset.id));// po tym można szukać argumentu dla tasklistRefresh (odpowiednika taskList.day)
+                deleteTaskObjFromTab(taskToDelete);
+                console.log(this);
+                taskListRefresh(this.parentElement.parentElement.dataset.day);//arg all?
+                TaskCounterRefresh();
+            });
+            i++;//przejscie w petli do kolejnego przycisku
+            getButtons[i].addEventListener("click", function (ev) {
+                //---zaznaczenie, ze zrobione
+                complete(this);
+                ev.stopImmediatePropagation();
+            });
+        }
+    };
+    //---zebranie do tablicy id zrobionych zadan
+    const doneTaskTabPreparation = function () {
+        const doneTasks = document.getElementsByClassName("done");
+        let doneTaskId = [];
+        for (let i = 0; i < doneTasks.length; i++) {
+            doneTaskId.push(parseInt(doneTasks[i].dataset.id));
+        }
+        return doneTaskId;
+    };
+//------------przygotowanie tablicy dni do odswiezenia-------------------
+    const daysToRefresh = function (doneTaskId) {
+        let daysToRefreshTab = [];
+        for (let i = 0; i < doneTaskId.length; i++) {
+            for (let j = 0; j < taskList.length; j++) {
+                if (doneTaskId[i] == taskList[j].id) {
+                    daysToRefreshTab.push(taskList[j].day);
+                }
+            }
+        }
+        return daysToRefreshTab;
+    };
+//----------------------------------------
+
+    //---przycisk usuwania zrobionych zadan
+    removeFinishedTasksBtn.addEventListener("click", function (daysToRefreshTab) {
+        let doneTasks = doneTaskTabPreparation();
+        let dToRefr = daysToRefresh(doneTasks);
+        deleteTaskObjFromTab(doneTasks);
+        taskListRefresh(dToRefr);
+        TaskCounterRefresh();
+    });
+    //---usuniecie z tablicy obiektow zadan wg przekazanych id
+    const deleteTaskObjFromTab = function (toCut) {
+        taskList = taskList.filter(item => !toCut.includes(item.id));
+    };
+    //-----obejscie problemu zmieniajacej sie dlugosci tablicy przy usuwaniu z niej elementow:
+    // https://stackoverflow.com/questions/5767325/how-do-i-remove-a-particular-element-from-an-array-in-javascript
+    // można dodać plyfill do includes: https://tc39.github.io/ecma262/#sec-array.prototype.includes
+    // polyifill do filter: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter#Polyfill
+
+    //---dodaje lub usuwa klase "done"
+    const complete = function (context) {
+        context.parentElement.classList.toggle("done");
+    };
+    //---przycisk dodawania zadania
+    addBtn.addEventListener("click", function () {
+        if (taskInput.value.length > 5 && taskInput.value.length < 100) {
+            if (Number.isInteger(parseInt(priorityInput.value)) && parseInt(priorityInput.value) >= 1 && parseInt(priorityInput.value) <= 10) {
+                if (Number.isInteger(parseInt(timeInput.value)) && parseInt(timeInput.value) >= 0.5 && checkTotalTime(daySelect.options[daySelect.options.selectedIndex].value) <= activityHours) {
+                    taskList.push(new Task(taskInput.value, priorityInput.value, idCounter, daySelect.options[daySelect.options.selectedIndex].value, timeInput.value));//new args 4 & 5
+                    idCounter++;
+                    taskListRefresh(daySelect.options[daySelect.options.selectedIndex].value);
+                    TaskCounterRefresh();
+                    inputsRefresh();
+                }
+                else {
+                    alert("ProTip: Pamiętaj, żeby się wyspać ;) \n Minimalny czas przewidzany na zadnie 0.5 godziny, \n maksymalny 16.");
+                }
+            }
+            else {
+                alert("Whooha! \n ProTip: Priorytet powinien być liczbą całkowitą z zakresu 1-10");
+            }
+        }
+        else {
+            alert("Whooha! \n ProTip: Minimum 5 znaków, nie więcej niż 100.");
+        }
+    });
+
+    //taskList tester na żądanie
+    document.addEventListener("keyup", function (ev) {
+        if (ev.which === 84) { //t key
+            ev.preventDefault();
+            console.log(taskList);
+        }
+    });
+
+    //wysokość okna  na żądanie
+    document.addEventListener("keyup", function (ev) {
+        if (ev.which === 72) { //h key
+            ev.preventDefault();
+            console.log(window.innerHeight);
+        }
+    });
+
+//przy dodawaniu taska sprawdzamy czy w danym dniu łączny czas tasków nie przekracza wartości activityHours
+
+    function checkTotalTime(whichDay) {
+        const currentlyAdded = parseInt(timeInput.value);
+        let totalTime = currentlyAdded;
+        for (let j = 0; j < taskList.length; j++) {
+            console.log("pętla w funkcji checkTotlaTime");
+            if (taskList[j].day === whichDay) {
+                totalTime += parseInt(taskList[j].time);
+                console.log("Spełniony warunek w funkcji checkTotlaTime");
+            }
+        }
+        console.log("Czas całkowity ", totalTime, typeof totalTime);
+        return totalTime;
+    }
+
+//----------obsługa-dragable------
+
+// generujemy eventy dla przeciągalnych tasków
+    function dragEventsOfTask() {
+        const tasks = document.querySelectorAll('.task');
+
+        for (const taskToDrag of tasks) {
+            taskToDrag.addEventListener('dragstart', dragStart);
+            taskToDrag.addEventListener('dragend', dragEnd);
+        }
+    }
+
+//generujemy eventy dla lądowisk (dni)
+    function dropAreaEvents(days) {
+        for (const day of days) {
+            day.addEventListener('dragover', dragOver);
+            day.addEventListener('dragenter', dragEnter);
+            day.addEventListener('dragleave', dragLeave);
+            day.addEventListener('drop', dragDrop);
+        }
+    }
+    dropAreaEvents(taskListContainer);
+
+
+// Drag Functions
+    function dragStart() {
+        dragging = this;
+        draggingId = dragging.dataset.id;
+        //console.log("dragging", dragging);
+        //this.className += ' hold';
+        //setTimeout(() => (this.className = 'invisible'), 0);
+        //console.log("dragStart");
+    }
+
+    function dragEnd() {
+        //this.className = 'fill';
+        //console.log("dragEnd");
+    }
+
+    function dragOver(e) {
+        e.preventDefault();
+        //console.log("dragOver");
+    }
+
+    function dragEnter(e) {
+        e.preventDefault();
+        //this.className += ' hovered';
+        //console.log("dragEnter");
+    }
+
+    function dragLeave() {
+       // this.className = 'empty';
+        //console.log("dragLeave");
+    }
+
+    function dragDrop() {
+        //this.className = 'empty';
+        this.append(dragging);
+       // console.log("dragDrop", this.dataset.day);
+        for (let i = 0; i < taskList.length; i++) {
+            if (taskList[i].id == draggingId) {
+                taskList[i].day = this.dataset.day;
+            }
+        }
+    }
+
+});
